a37055dee0202d088b8b55b0b51957c6a0d13e4b
alvin <alvin@buuuk.com>
2013-03-18 07:14:12 +0800

    basic feature complete

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import hashlib
 import web
 import db
+import re
 
+from pretty_date import pretty_date
 from models import *
 
 urls = (
@@ -14,17 +16,16 @@ urls = (
   
   # topic
   '/topic/new', 'new_topic',    
-  '/topic/upvote', 'topic', # upvote story
   '/topic/(\d+)', 'topic',
+  '/topic/(\d+)/upvote', 'upvote_topic', # upvote story
   '/topic/(\d+)/base/new', 'new_base_link',  # add base story
   '/topic/(\d+)/alt/new',  'new_alt_link',  # add alt story
   
   # link
   '/link/(\d+)', 'link',  # add base story
-  '/link/(\d+)/upvote', 'link',  # add base story
-  # comment
-  '/link/(\d+)/comment', 'new_comment',
+  '/link/(\d+)/upvote', 'upvote_link',  # add base story
 
+  '/comment/(\d+)/upvote', 'upvote_comment',  # add base story
 
   '/about', 'about',
   '/faq', 'faq',
@@ -32,20 +33,32 @@ urls = (
 
 )
 
+
 def load_sqla(handler):
-    try:
-        return handler()
-    except web.HTTPError:
-       db.session.commit()
-       raise
-    except:
-        db.session.rollback()
-        raise
-    finally:
-        db.session.commit()
-        # If the above alone doesn't work, uncomment 
-        # the following line:
-        #db.session.expunge_all() 
+  try:
+      return handler()
+  except web.HTTPError:
+     db.session.commit()
+     raise
+  except:
+      db.session.rollback()
+      raise
+  finally:
+      db.session.commit()
+      # If the above alone doesn't work, uncomment 
+      # the following line:
+      #db.session.expunge_all() 
+
+def shorten_link(link):
+  l = re.findall(r"^[^\/]+:\/\/[^\/]*?\.?([^\/.]+)\.[^\/.]+(?::\d+)?\/?", link)
+
+  if len(l) > 0:
+    e = re.findall(r"%s\.(..[a-zA-Z]?)/?" % l[0], link)
+    return l[0]+"."+e[0]
+  else:
+    return link
+
+
 
 app = web.application(urls, globals())
 app.add_processor(load_sqla)
@@ -61,16 +74,18 @@ else:
 
 
 ## home page
-class index:        
+class index:  
+  def all_topics(self):
+    return db.session.query(Topic).all()
+
   def GET(self):
-    render = web.template.render('templates/', base='layout')
-    username = False
+    render = web.template.render('templates/', base='layout', globals={'session':session,'pretty_date':pretty_date})
+    username = None
     if hasattr(session,'username'):
       username = session.username
-    else:
-      username = False
-    web.debug(username)
-    return render.index(username)
+    
+    topics = self.all_topics()
+    return render.index(topics)
 
 
 
@@ -106,18 +121,19 @@ class login:
   )
 
   def GET(self):
+    session.kill()
     url = web.input(url='').url
     login_form = self.login_form()
     login_form.fill({'url':url})
 
-    render = web.template.render('templates/', base="layout")
+    render = web.template.render('templates/', base='layout', globals={'session':session,'pretty_date':pretty_date})
     return render.user.login(login_form, "Log in")
 
   def POST(self):
     i = web.input()
     login_form = self.login_form()
     login_success = self.valid_email_password(i.username, i.password)
-    render = web.template.render('templates/', base="layout")
+    render = web.template.render('templates/', base='layout', globals={'session':session,'pretty_date':pretty_date})
 
     if (not login_form.validates()) or (not login_success):
       return render.user.login(login_form, "Log in")
@@ -171,7 +187,7 @@ class register:
       description="repeat password:"),
     web.form.Button('sign up'),
     validators = [
-        web.form.Validator("Passwords did'nt match", lambda i: i.password == i.password2)]
+        web.form.Validator("Passwords didn't match", lambda i: i.password == i.password2)]
   )
 
 
@@ -181,7 +197,7 @@ class register:
     registration_form = self.registration_form()
     registration_form.fill({'url':url})
 
-    render = web.template.render('templates/', base="layout")
+    render = web.template.render('templates/', base='layout', globals={'session':session,'pretty_date':pretty_date})
     return render.user.login(registration_form, "Sign up")
 
 
@@ -189,7 +205,7 @@ class register:
     i = web.input()
 
     registration_form = self.registration_form()
-    render = web.template.render('templates/', base="layout")
+    render = web.template.render('templates/', base='layout', globals={'session':session,'pretty_date':pretty_date})
 
     if not registration_form.validates():
       return render.user.login(registration_form, "Sign up")
@@ -203,6 +219,7 @@ class register:
 
       session.logged_in = True
       session.username = u.name
+      session.user = user
 
       web.seeother('/')
 
@@ -214,82 +231,187 @@ class register:
 
 ## new topics
 class new_topic:
-  def not_page_exists(url):
-    return True
+  def not_title_exists(title):
+    topic = db.session.query(Topic).filter_by(title=title).first()
+    if not topic:
+      return True
+    else:
+      return False
 
-  topic_exists_validator = web.form.Validator('Page already exists', 
-                                not_page_exists)
+  def not_hashtag_exists(hashtag):
+    topic = db.session.query(Topic).filter_by(hashtag=hashtag).first()
+    if not topic:
+      return True
+    else:
+      return False
+
+  title_exists_validator = web.form.Validator('Title already exists', 
+                                not_title_exists)
+  hashtag_exists_validator = web.form.Validator('Hashtag already exists', 
+                                not_hashtag_exists)
+
+  vhashtag = web.form.regexp(r"#.*", "must be a valid hashtag start with #")
 
   form = web.form.Form(
-      web.form.Textbox('title', web.form.notnull, topic_exists_validator,
+      web.form.Textbox('title', web.form.notnull, title_exists_validator,
           size=30,
           description="title:"),
-      web.form.Textbox('hashtag', web.form.notnull, 
+      web.form.Textbox('hashtag', web.form.notnull, vhashtag, hashtag_exists_validator,
           size=30,
-          placeholder="#something",
+          placeholder="#topic_hashtag", 
           description="hashtag:"),
       web.form.Button('Create topic'),
   )
 
   def GET(self):
+    username = None
+    if hasattr(session, 'username'):
+      username = session.username
+    else:
+      return web.seeother('/register')
+
     url = web.input(url='').url
     form = self.form()
     form.fill({'url':url})
 
-    render = web.template.render('templates/', base='layout')
+    render = web.template.render('templates/', base='layout', globals={'session':session,'pretty_date':pretty_date})
+
+
     return render.topic.new(form)
+
+  def POST(self):
+    username = None
+    if hasattr(session, 'username'):
+      username = session.username
+    else:
+      return web.seeother('/register')
+
+    i = web.input()
+
+    form = self.form()
+    render = web.template.render('templates/', base='layout', globals={'session':session,'pretty_date':pretty_date})
+
+    if not form.validates():
+      return render.topic.new(form)
+    else:
+      topic = Topic(title=i.title, hashtag=i.hashtag, user_id=session.user.id)
+      db.session.add(topic)
+      db.session.commit()
+
+      web.seeother('/topic/%d' % topic.id)
+
 
 
 
 ## topic 
 class topic:
+  def topic(self, id):
+    return db.session.query(Topic).filter_by(id=id).first()
+
   def GET(self, id):
-    render = web.template.render('templates/', base='layout')
-    return render.topic.show(id)
+    render = web.template.render('templates/', base='layout', globals={'session':session, 'short': shorten_link, 'pretty_date': pretty_date })
+    return render.topic.show(id, self.topic(id))
+
+## upvote topic
+class upvote_topic:
+  def GET(self, id):
+    username = None
+    if hasattr(session, 'username'):
+      username = session.username
+    else:
+      return web.seeother('/register')
+
+    voted = db.session.query(TopicVote).filter_by(topic_id=id, user_id=session.user.id).first()
+
+    if not voted:
+      vote = TopicVote(topic_id=id, user_id=session.user.id)
+      db.session.add(vote)
+
+    return web.seeother("/topic/%d" % int(id))
+
+
 
 
 
 ## new base link
 class new_base_link:
   def not_link_exists(url):
-    return True
+    link = db.session.query(Link).filter_by(url=url).first()
+    if not link:
+      return True
+    else:
+      return False
 
-  link_exists_validator = web.form.Validator('Page already exists', 
+  link_exists_validator = web.form.Validator('Link already exists', 
                                 not_link_exists)
 
+  vlink = web.form.regexp(r"https?://.+\..+", "must be a valid url start with http(s)")
+
   form = web.form.Form(
-      web.form.Textbox('title', web.form.notnull, link_exists_validator,
+      web.form.Textbox('title', web.form.notnull, 
           size=30,
           description="title:"),
-      web.form.Textbox('url', web.form.notnull,
+      web.form.Textbox('url', web.form.notnull, vlink, link_exists_validator,
           size=30,
           description="url:"),
       web.form.Button('add base story link'),
   )
 
   def GET(self, id):
+    username = None
+    if hasattr(session, 'username'):
+      username = session.username
+    else:
+      return web.seeother('/register')
+
     url = web.input(url='').url
     form = self.form()
     form.fill({'url':url})
 
-    render = web.template.render('templates/', base='layout')
+    render = web.template.render('templates/', base='layout', globals={'session':session,'pretty_date':pretty_date})
     return render.link.new(form, id)
+
+  def POST(self, id):
+    username = None
+    if hasattr(session, 'username'):
+      username = session.username
+    else:
+      return web.seeother('/register')
+
+    i = web.input()
+
+    form = self.form()
+    render = web.template.render('templates/', base='layout', globals={'session':session,'pretty_date':pretty_date})
+
+    if not form.validates():
+      return render.link.new(form,id)
+    else:
+      link = Link(title=i.title, url=i.url, topic_id=id, user_id=session.user.id, type=1)
+      db.session.add(link)
+      db.session.commit()
+
+      return web.seeother('/topic/%d' % int(id))
 
 
 
 ## new alternate link
 class new_alt_link:
   def not_link_exists(url):
-    return True
+    link = db.session.query(Link).filter_by(url=url).first()
+    if not link:
+      return True
+    else:
+      return False
 
-  link_exists_validator = web.form.Validator('Page already exists', 
+  link_exists_validator = web.form.Validator('Link already exists', 
                                 not_link_exists)
+  vlink = web.form.regexp(r"https?://.+\..+", "must be a valid url start with http(s)")
 
   form = web.form.Form(
-      web.form.Textbox('title', web.form.notnull, link_exists_validator,
+      web.form.Textbox('title', web.form.notnull, 
           size=30,
           description="title:"),
-      web.form.Textbox('url', web.form.notnull,
+      web.form.Textbox('url', web.form.notnull, vlink, link_exists_validator,
           size=30,
           description="url:"),
       web.form.Button('add alternate story link'),
@@ -300,50 +422,144 @@ class new_alt_link:
     form = self.form()
     form.fill({'url':url})
 
-    render = web.template.render('templates/', base='layout')
+    render = web.template.render('templates/', base='layout', globals={'session':session,'pretty_date':pretty_date})
     return render.link.new(form,id)
+
+  def POST(self, id):
+    username = None
+    if hasattr(session, 'username'):
+      username = session.username
+    else:
+      return web.seeother('/register')
+
+    i = web.input()
+
+    form = self.form()
+    render = web.template.render('templates/', base='layout', globals={'session':session,'pretty_date':pretty_date})
+
+    if not form.validates():
+      return render.link.new(form, id)
+    else:
+      link = Link(title=i.title, url=i.url, topic_id=id, user_id=session.user.id, type=2)
+      db.session.add(link)
+      db.session.commit()
+
+      return web.seeother('/topic/%d' % int(id))
 
 
 
 
 ## link
 class link:
+  form = web.form.Form(
+      web.form.Textarea('comment', web.form.notnull),
+  )
+
+  def link(self, id):
+    return db.session.query(Link).filter_by(id=id).first()
+
   def GET(self, id):
-    render = web.template.render('templates/', base='layout')
-    return render.link.show(id)
+    i = web.input()
 
+    form = self.form()
+    render = web.template.render('templates/', base='layout', globals={'session':session, 'short': shorten_link, 'pretty_date':pretty_date})
+    return render.link.show(form, id, self.link(id))
 
-
-## new_comment
-class new_comment:
   def POST(self, id):
-    return web.seeother("/link/id")
+    username = None
+    if hasattr(session, 'username'):
+      username = session.username
+    else:
+      return web.seeother('/register')
+
+    i = web.input()
+
+    form = self.form()
+    render = web.template.render('templates/', base='layout', globals={'session':session, 'short': shorten_link, 'pretty_date':pretty_date})
+
+    if not form.validates():
+      return render.link.show(form, id, self.link(id))
+    else:
+      comment = Comment(content=i.comment, link_id=id, user_id=session.user.id)
+      db.session.add(comment)
+      db.session.commit()
+
+      return web.seeother('/link/%d' % int(id))
+
+
+
+
+
+
+
+## upvote link
+class upvote_link:
+  def GET(self, id):
+    username = None
+    if hasattr(session, 'username'):
+      username = session.username
+    else:
+      return web.seeother('/register')
+
+    voted = db.session.query(LinkVote).filter_by(link_id=id, user_id=session.user.id).first()
+
+    if not voted:
+      vote = LinkVote(link_id=id, user_id=session.user.id)
+      db.session.add(vote)
+
+    return web.seeother("/link/%d" % int(id))
+
+
+
+
+## upvote comment
+class upvote_comment:
+  def GET(self, id):
+    username = None
+    if hasattr(session, 'username'):
+      username = session.username
+    else:
+      return web.seeother('/register')
+
+    voted = db.session.query(CommentVote).filter_by(comment_id=id, user_id=session.user.id).first()
+
+    if not voted:
+      vote = CommentVote(comment_id=id, user_id=session.user.id)
+      db.session.add(vote)
+
+    comment = db.session.query(Comment).filter_by(id=id).first()
+
+    return web.seeother("/link/%d" % int(comment.link_id))
+
+
 
 
 
 ## user
 class user:
   def GET(self,id):
-    render = web.template.render('templates/', base='layout')
+    render = web.template.render('templates/', base='layout', globals={'session':session,'pretty_date':pretty_date})
     return render.user.show(id)
-
-
 
 ## static pages
 class about:
   def GET(self):
-    render = web.template.render('templates/', base='layout')
+    render = web.template.render('templates/', base='layout', globals={'session':session,'pretty_date':pretty_date})
     return render.static.about()
 
 class faq:
   def GET(self):
-    render = web.template.render('templates/', base='layout')
+    render = web.template.render('templates/', base='layout', globals={'session':session,'pretty_date':pretty_date})
     return render.static.faq()
 
 class contact:
   def GET(self):
-    render = web.template.render('templates/', base='layout')
+    render = web.template.render('templates/', base='layout', globals={'session':session,'pretty_date':pretty_date})
     return render.static.contact()
+
+
+
+
 
 if __name__ == "__main__":
     app.run()
--- models.py
+++ models.py
@@ -1,24 +1,241 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, DateTime, String, Boolean, ForeignKey, distinct, UniqueConstraint, Text
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.expression import func
+from sqlalchemy.orm import relationship, backref, object_session
+from sqlalchemy.orm.interfaces import MapperExtension 
+import db
+from datetime import datetime
 
 Base = declarative_base()
 
+
+class BaseExtension(MapperExtension):  
+
+  def before_insert(self, mapper, connection, instance):  
+      instance.created_at = datetime.now()  
+
+  def before_update(self, mapper, connection, instance):  
+      instance.updated_at = datetime.now()
+
+
 class User(Base):
-    __tablename__ = 'users'
+  __tablename__ = 'users'
+  __mapper_args__ = { 'extension': BaseExtension() }
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(256))
-    email = Column(String(256))
-    password = Column(String(256))
-    active = Column(Boolean(256))
+  id = Column(Integer, primary_key=True)
+  name = Column(String(256))
+  email = Column(String(256))
+  password = Column(String(256))
+  active = Column(Boolean, default=True)
+  created_at = Column(DateTime) 
+  updated_at = Column(DateTime) 
 
-    def __init__(self, name, email, password):
-        self.name = name
-        self.email = email
-        self.password = password
+  def __init__(self, name, email, password):
+    self.name = name
+    self.email = email
+    self.password = password
 
-    def __repr__(self):
-       return "<User('%s','%s', '%s')>" % (self.name, self.email, self.password)
+  def __repr__(self):
+    return "<User('%s','%s', '%s')>" % (self.name, self.email, self.password)
+
+
+
+
+
+class Topic(Base):
+  __tablename__ = 'topics'
+  __mapper_args__ = { 'extension': BaseExtension() }
+
+  id = Column(Integer, primary_key=True)
+  title = Column(String(256))
+  hashtag = Column(String(256))
+  user_id = Column(Integer, ForeignKey('users.id'))
+  active = Column(Boolean, default=True)
+  created_at = Column(DateTime) 
+  updated_at = Column(DateTime) 
+
+  user = relationship("User")
+
+  def __init__(self, title, hashtag, user_id):
+    self.title = title
+    self.hashtag = hashtag
+    self.user_id = user_id
+
+  def __repr__(self):
+     return "<Topic('%s','%s', %d)>" % (self.title, self.hashtag, self.user_id)
+    
+  def _get_points(self):
+    return object_session(self).query(distinct(TopicVote.user_id)).filter_by(topic_id=self.id).count()
+  
+  points = property(_get_points)
+
+  def _get_base_stories(self):
+    return object_session(self).query(Link).filter_by(topic_id=self.id, type=1).all()
+  
+  base_stories = property(_get_base_stories)
+
+  def _get_alt_stories(self):
+    return object_session(self).query(Link).filter_by(topic_id=self.id, type=2).all()
+  
+  alt_stories = property(_get_alt_stories)
+
+  def _get_story_count(self):
+    return object_session(self).query(Link).filter_by(topic_id=self.id).count()
+  
+  story_count = property(_get_story_count)
+
+
+
+
+
+
+class TopicVote(Base):
+  __tablename__ = 'topic_votes'
+  __mapper_args__ = { 'extension': BaseExtension() }
+
+  id = Column(Integer, primary_key=True)
+  user_id = Column(Integer, ForeignKey('users.id'))
+  topic_id = Column(Integer, ForeignKey('topics.id'))
+
+  user = relationship("User")
+  topic = relationship("Topic")
+  created_at = Column(DateTime) 
+  updated_at = Column(DateTime) 
+
+  UniqueConstraint('user_id', 'topic_id', name='one_vote_per_user')
+
+  def __init__(self, topic_id, user_id):
+    self.topic_id = topic_id
+    self.user_id = user_id
+
+  def __repr__(self):
+     return "<TopicVote(%d, %d)>" % (self.topic_id, self.user_id)
+
+
+
+
+
+class Link(Base):
+  __tablename__ = 'links'
+  __mapper_args__ = { 'extension': BaseExtension() }
+
+  id = Column(Integer, primary_key=True)
+  title = Column(String(256))
+  url = Column(String(256))
+  user_id = Column(Integer, ForeignKey('users.id'))
+  topic_id = Column(Integer, ForeignKey('topics.id'))
+  type = Column(Integer)
+  active = Column(Boolean, default=True)
+  created_at = Column(DateTime) 
+  updated_at = Column(DateTime) 
+
+  user = relationship("User")
+  topic = relationship("Topic")
+
+  def __init__(self, title, url, user_id, topic_id, type):
+    self.title = title
+    self.url = url
+    self.user_id = user_id
+    self.topic_id = topic_id
+    self.type = type
+
+  def __repr__(self):
+     return "<Link('%s','%s', %d, %d)>" % (self.title, self.url, self.user_id, self.topic_id)
+
+  def _get_points(self):
+    return object_session(self).query(distinct(LinkVote.user_id)).filter_by(link_id=self.id).count()
+  
+  points = property(_get_points)
+
+  def _get_comments_count(self):
+    return object_session(self).query(Comment).filter_by(link_id=self.id).count()
+  
+  comments_count = property(_get_comments_count)
+
+  def _get_comments(self):
+    return object_session(self).query(Comment).filter_by(link_id=self.id).all()
+  
+  comments = property(_get_comments)
+
+
+
+class LinkVote(Base):
+  __tablename__ = 'link_votes'
+  __mapper_args__ = { 'extension': BaseExtension() }
+
+  id = Column(Integer, primary_key=True)
+  user_id = Column(Integer, ForeignKey('users.id'))
+  link_id = Column(Integer, ForeignKey('links.id'))
+  created_at = Column(DateTime) 
+  updated_at = Column(DateTime) 
+
+  user = relationship("User")
+  link = relationship("Link")
+
+  def __init__(self, link_id, user_id):
+    self.link_id = link_id
+    self.user_id = user_id
+
+  def __repr__(self):
+     return "<LinkVote(%d, %d)>" % (self.link_id, self.user_id)
+
+
+
+
+
+
+class Comment(Base):
+  __tablename__ = 'comments'
+  __mapper_args__ = { 'extension': BaseExtension() }
+
+  id = Column(Integer, primary_key=True)
+  content = Column(Text(256))
+  user_id = Column(Integer, ForeignKey('users.id'))
+  link_id = Column(Integer, ForeignKey('links.id'))
+  active = Column(Boolean, default=True)
+  created_at = Column(DateTime) 
+  updated_at = Column(DateTime) 
+
+  user = relationship("User")
+  link = relationship("Link")
+
+  def __init__(self, content, user_id, link_id):
+    self.content = content
+    self.user_id = user_id
+    self.link_id = link_id
+
+  def __repr__(self):
+     return "<Link('%s', %d, %d)>" % (self.content, self.user_id, self.link_id)
+
+  def _get_points(self):
+    return object_session(self).query(distinct(CommentVote.user_id)).filter_by(link_id=self.id).count()
+  
+  points = property(_get_points)
+
+
+
+
+class CommentVote(Base):
+  __tablename__ = 'comment_votes'
+  __mapper_args__ = { 'extension': BaseExtension() }
+
+  id = Column(Integer, primary_key=True)
+  user_id = Column(Integer, ForeignKey('users.id'))
+  comment_id = Column(Integer, ForeignKey('comments.id'))
+  created_at = Column(DateTime) 
+  updated_at = Column(DateTime) 
+
+  user = relationship("User")
+  comment = relationship("Comment")
+
+  def __init__(self, comment_id, user_id):
+    self.comment_id = comment_id
+    self.user_id = user_id
+
+  def __repr__(self):
+     return "<LinkVote(%d, %d)>" % (self.user_id, self.comment_id)
+
+
 
 
 users_table = User.__table__
