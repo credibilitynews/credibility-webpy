fc165acd4cb23c2bcb67a8bdcdc28d280a9e84be
alvinsj <alvin.nsj@gmail.com>
2014-03-22 19:37:21 +0700

    added rss feed!

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import web
 import db
 import re
 import collections
+import requests
+import json
+
+from bs4 import BeautifulSoup
 
 from pretty_date import pretty_date
 
@@ -16,6 +20,7 @@ from sqlalchemy import desc
 ## routes
 urls = (
   '/', 'index',
+  '/feed', 'feed',
 
   '/login', 'login',
   '/register', 'register',
@@ -102,9 +107,57 @@ class favicon:
         raise web.redirect('/static/favicon.ico')
 
 
-import json
-import requests
-from bs4 import BeautifulSoup
+class Struct:
+    def __init__(self, **entries): 
+        self.__dict__.update(entries)
+
+
+
+import time
+from email.Utils import formatdate
+## rss feed
+class feed:
+    def latest_articles(self):
+        return db.session.query(Link).order_by(desc(Link.created_at)).limit(10)
+
+    def last_link_date(self):
+        last_link = db.session.query(Link).order_by(desc(Link.created_at)).limit(1)
+        return self.format_datetime(last_link[0].created_at)
+
+    def topic_link(self, link):
+        return '<a href="http://credibility.cc/topic/'+str(link.topic.id)+'">'+link.topic.title+' '+link.topic.hashtag+'</a>'
+
+    def root_link(self):
+        return '<a href="http://credibility.cc">Credibility.cc</a>'
+
+    def format_datetime(self, datetime):
+        return formatdate(time.mktime(datetime.timetuple()))
+
+    def feed_posts(self):
+        articles = self.latest_articles();
+        posts = []
+        for article in articles:
+            post = {}
+            post['title'] = article.title+' '+article.topic.hashtag
+            post['link'] = article.url
+            post['body'] = 'More articles on '+self.topic_link(article)+' available at '+self.root_link()
+            post['category'] = ' > '.join([str(tag.name) for tag in article.topic.tags])
+            post['comments'] = 'http://credibility.cc/link/'+str(article.id)
+            post['pub_date'] = self.format_datetime(article.created_at)
+            post['guid'] = 'http://credibility.cc/link/'+str(article.id)
+            posts.append(Struct(**post))
+        return posts
+
+    def GET(self):
+        render = web.template.render('templates/')
+        date = self.last_link_date()
+        posts = self.feed_posts()
+        category = "Latest Links"
+        web.header('Content-Type', 'application/xml')
+        return render.feed(date, category, posts)
+
+
+
 ## suggest title for url
 class suggest_title: 
     def title_from_url(self, url):
